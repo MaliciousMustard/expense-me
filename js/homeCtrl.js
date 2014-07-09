9efaf9ec280b171c712ed64a4f7b61412fc05cd5
Alex Ntousias <antousias@gmail.com>
2014-07-09 02:40:34 +0100

    Show notification when clicking item with default price, otherwise show dialog box

--- js/homeCtrl.js
+++ js/homeCtrl.js
@@ -9,4 +9,39 @@ expenseMeApp.controller('HomeCtrl', function($scope) {
 	for (i in categories) {
 		$scope.iconPerCategory[categories[i].name] = categories[i].icon;
 	}
+	
+	$scope.registerExpense = function(item) {
+		if (item.price) {
+			alertBox.show(item.name + ' registered');
+		} else {
+			bootbox.prompt("Price for " + item.name, function(price) {
+				if (price && !isNaN(price)) {
+					alertBox.show(item.name + ' registered');
+				} else {
+					return false;
+				}
+			});
+		}
+	};
 });
+
+// The following code is taken from bootboxjs.com
+var alertBox = (function() {
+	"use strict";
+
+	var elem, hideHandler, that = {};
+
+	that.init = function(options) {
+		elem = $(options.selector);
+	};
+
+	that.show = function(text) {
+		clearTimeout(hideHandler);
+
+		elem.find("span").html(text);
+		elem.delay(200).fadeIn().delay(2500).fadeOut();
+	};
+
+	return that;
+}());
+// end of bootboxjs.com snippet
